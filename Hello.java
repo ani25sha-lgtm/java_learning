dfe41d079c9d98ce7e166504abc1ec820e128db5
Anisha <ani25sha@gmail.com>
2026-08-08 21:56:14 +0530

    print and prinln diff

--- Hello.java
+++ Hello.java
@@ -1,6 +1,13 @@
 
 public class Hello{  
-    public static void main(String args[]){  
-     System.out.println("Hello World!");  
-    }  
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+
+        System.out.print("no new line -->");
+        System.out.println("  as u can see");
+
+        System.out.println("new line added at the end");
+        System.out.println("see the difference");
+        System.out.println("we prefer println");
+    }
    }
---
+++ variable.java
@@ -0,0 +1,13 @@
+
+
+/*variable : 
+3 types :-
+ 1.Local--> decared and access inside body of a method only|Not static|not shared among methods.
+ 2 Instance--> decared and access inside class and outside the body of a method|Not static|not shared among instances.
+ 3. Static--> static|can't be local|can create a single copy and share it among all instances.
+ */
+
+//
+public class variable {
+}
+class sum
