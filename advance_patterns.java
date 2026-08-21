36462a7d1b71dccaf28ba52bfbc124b47ee6ecc7
Anisha <ani25sha@gmail.com>
2026-08-21 21:44:59 +0530

    modified

--- advance_patterns.java
+++ advance_patterns.java
@@ -55,5 +55,14 @@ public class advance_patterns {
 
        // n=5;
         System.out.println("pattern 3:");
+        for(int i=0;i<n;i++){
+            for(int k=n-i-1;k>0;k--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<n;j++){
+                System.out.print("*");
+            }
+        System.out.println();
+        }
     }
 }
